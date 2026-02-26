c110f3a66071bcc9b667cafad2f8ae5c386300e3
TONYP <anthonypinheiro39@gmail.com>
2026-02-26 11:43:29 -0300

    Add files via upload

---
+++ Desafio1.py
@@ -0,0 +1,12 @@
+print(" A catraca esta travada, escolha entre inserir moeda ,Empurrar")
+Catraca = False
+while True :
+    ação1 = input(" Escolha entre  moeda , Empurrar")
+    if ação1 == "Empurrar":
+    print("Voce nao pode passar canalha")
+  
+
+
+
+
+    break

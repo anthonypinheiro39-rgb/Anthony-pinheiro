849037326fd8bd77721ac9550aa577313deb13db
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2026-03-19 18:31:47 -0300

    Update and rename Salario.py to Atividade05.py

--- AtividadeComplementar/Atividade05.py
+++ AtividadeComplementar/Atividade05.py
@@ -1,4 +1,4 @@
 nome = input('Qual seu Nome :')
 salario = int(input('Qual seu Salario :'))
-aumento = (salario + (salario *10/100))
+aumento = (salario + (salario * 15/100))
 print ('Olá {} Ja que seu salario é {} então o seu aumento será {}'.format(nome, salario,aumento))

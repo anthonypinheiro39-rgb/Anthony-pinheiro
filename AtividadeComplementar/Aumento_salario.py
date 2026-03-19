3b49e8092c2414535bdcf535152c99b6fd6a8438
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2026-03-19 18:32:23 -0300

    Delete AtividadeComplementar/Aumento_salario.py

--- AtividadeComplementar/Aumento_salario.py
+++
@@ -1,5 +0,0 @@
-nome = input ('Digite seu nome : ')
-salario = float (input( ' Digite seu salario $ : '))
-aumento = salario + ( salario * 15 /100)
-print (f' Ola {nome} com o novo aumento seu salario será  {aumento}   ')
-
